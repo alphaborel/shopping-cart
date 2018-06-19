2ae648663c24ca9b31aa40d067c8247b823a97cb
alphaborel <aaborel@gmail.com>
2018-06-19 10:54:27 -0700

    added list items to cart

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import CartItems from './components/CartItems';
 
 class App extends Component {
   render() {
+    var cartItemsList = [
+  { id: 1, name: 'Mediocre Iron Watch', priceInCents: 3.99, quantity: 1 },
+  { id: 2, name: 'Heavy Duty Concrete Plate', priceInCents: 4.99 , quantity: 2 },
+  { id: 3, name: 'Intelligent Paper Knife', priceInCents: 19.99 , quantity: 1 },
+]
     return (
       <div>
       <CartHeader />
-      <CartItems />
+      <CartItems items={cartItemsList} />
       <CartFooter copy="&copy; 2016"/>
       </div>
     );
--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
 
-const somefunc = () => {
-  console.log('Im here!');
-}
-
 class CartItems extends Component {
 
   render() {
-
+    let theList = this.props.items;
     return (
       <div className="container">
       <h1>Cart Items</h1>
+      <div className="list-group">
+        <div className="list-group-item">
+          <div className="row">
+            <div className="col-md-8">Product</div>
+            <div className="col-md-2">Price</div>
+            <div className="col-md-2">Quantity</div>
+        </div>
+      </div>
+      <div class="collection-item">
+    { theList.map((theList)=>
+
+      <div class="row">
+      <div class="col-md-8">{theList.name}</div>
+      <div class="col-md-2">{theList.priceInCents}</div>
+      <div class="col-md-2">{theList.quantity}</div>
+      </div>
+
+     )}
+      </div>
+      </div>
       </div>
     );
   }
-  // somefunc = () => {
-  //   console.log('Im here!');
-  // }
-
 }
-
 
 export default CartItems;
